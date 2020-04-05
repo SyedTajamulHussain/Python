851a456086c205fbf2ca47fcd8c2e4fc61ba3e7e
SyedTajamulHussain <tajamulsyed26@yahoo.com>
2020-04-06 00:59:34 +0530

    Data types

---
+++ PythonTutorial/DataType.py
@@ -0,0 +1,24 @@
+a=10
+print(a)
+a="Tajamul"
+b = " Hussain"
+c = a+ b
+print (c)
+
+x=11.11
+y=x
+print(x)
+print(y)
+print(id(x))
+print(id(y))
+
+Name =" Syed Tajamul Hussain"
+Age = 38
+print(Name,Age)
+Name, Age,Ocuupation = "Syed Zahid",36,"QA LEAD"
+print(Name,Age,Ocuupation)
+u=v=t=10
+print(u,v,t)
+
+S1= "Syed tajamul Hussain"
+print(S1[0:4])
