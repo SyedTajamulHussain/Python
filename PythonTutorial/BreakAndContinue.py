cd1d4f139b5066e8f14d550335c60d44e86c0983
SyedTajamulHussain <tajamulsyed26@yahoo.com>
2020-04-11 23:38:39 +0530

    Breaka nd continue

---
+++ PythonTutorial/BreakAndContinue.py
@@ -0,0 +1,19 @@
+str1 = ["JAVA", "dot net", "phyton"]
+for i in range(len(str1)):
+    print(str1[i])
+    if str1[i] == "JAVA":
+        print("hey i found java")
+        break
+
+name = "Tajamul"
+for i in name:
+    print(i)
+    if(i=='m'):
+        break
+
+name = "Tajamul"
+for i in name:
+    print(i)
+    if i == 'm':
+        continue
+
