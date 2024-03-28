d19b1cdb70f3a03e254cc3d2072d7c3bccc09e61
SyedTajamulHussain <tajamulsyed26@gmail.com>
2024-03-29 00:11:55 +0530

    Day 2 - list

---
+++ PythonTutorial/basicPrograms/pythonLists.py
@@ -0,0 +1,11 @@
+# Lists are used to store multiple items in a single variable
+# List is a collection which is ordered and changeable. Allows duplicate members.
+# Tuple is a collection which is ordered and unchangeable. Allows duplicate members.
+# Set is a collection which is unordered, unchangeable*, and unindexed. No duplicate members.
+# Dictionary is a collection which is ordered** and changeable. No duplicate members.
+exampleList = ["Glass", "Cup", "plate","Glass"]
+print(exampleList)
+print(len(exampleList))
+# A list can contain different data types:
+exampleListTwo = [40,"Syed Hussain",5.1, 'male']
+print(exampleListTwo)
--- PythonTutorial/basicPrograms/pythonOperators.py
+++ PythonTutorial/basicPrograms/pythonOperators.py
@@ -10,7 +10,7 @@ Identity operators
 Membership operators
 Bitwise operators """
 
-# Arithmetic operators
+# Arithmetic operators are used with numeric values to perform common mathematical operations
 price = 25
 GST = 3.4
 Total = price + GST
@@ -20,3 +20,18 @@ print(2 * 2)
 print(4 / 2)
 print(5 % 10)
 print(4 // 2)
+
+# Assignment operators are used to assign values to variables:
+A = 5
+print(A)
+A += 5  # same as A = A + 3
+print(A)
+A -= 5  # same as A = A - 3
+print(A)
+A >>= 3
+print(A)
+A <<= 3
+print(A)
+
+# Python Comparison Operators are used to compare two values:
+
